ceb4f3e1a594cb62058e40bdffdcf5eac803427c
Farishy25 <58514816+Farishy25@users.noreply.github.com>
2019-12-12 14:44:19 +0700

    Update Menu.php

--- application/views/Template/Menu.php
+++ application/views/Template/Menu.php
@@ -28,11 +28,11 @@
       <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
 
-          <a class = "collapse-item" href = "<?= base_url('operator') ?>">Operator</a>
-          <a class = "collapse-item" href = "<?= base_url('usergroup') ?>">Usergroup</a>
-          <a class = "collapse-item" href = "<?= base_url('user') ?>">User</a>
-          <a class = "collapse-item" href = "<?= base_url('tipebarang') ?>">Tipe Barang</a>
-          <a class = "collapse-item" href = "<?= base_url('bagian') ?>">Bagian</a>
+          <a class="collapse-item" href="<?= base_url('operator') ?>">Operator</a>
+          <a class="collapse-item" href="<?= base_url('usergroup') ?>">Usergroup</a>
+          <a class="collapse-item" href="<?= base_url('user') ?>">User</a>
+          <a class="collapse-item" href="<?= base_url('tipebarang') ?>">Tipe Barang</a>
+          <a class="collapse-item" href="<?= base_url('bagian') ?>">Bagian</a>
 
         </div>
       </div>
@@ -46,18 +46,17 @@
     </a>
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
       <div class="bg-white py-2 collapse-inner rounded">
-        <?php if ($_SESSION['Admin']->id_level == 1 || $_SESSION['Admin']->id_level == 2) { ?>
+        <?php if ($_SESSION['Admin']->id_level == 1 || $_SESSION['Admin']->id_level == 2 || $_SESSION['Admin']->id_level == 3) { ?>
+          <a class="collapse-item" href="<?= base_url('barang') ?>">Barang</a>
+          <a class="collapse-item" href="<?= base_url('barangmasuk') ?>">Barang Masuk</a>
+          <a class="collapse-item" href="<?= base_url('barangkeluar ') ?>">Barang Keluar</a>
+        <?php } ?>
+        <!--
+        
           <a class = "collapse-item" href = "<?= base_url('barang') ?>">Barang</a>
           <a class = "collapse-item" href = "<?= base_url('barangmasuk') ?>">Barang Masuk</a>
           <a class = "collapse-item" href = "<?= base_url('barangkeluar ') ?>">Barang Keluar</a>
-        <?php } ?>
-
-        <?php if ($_SESSION['Admin']->id_level == 3) { ?>
-          <a class = "collapse-item" href = "<?= base_url('barang') ?>">Barang</a>
-          <a class = "collapse-item" href = "<?= base_url('barangmasuk') ?>">Barang Masuk</a>
-          <a class = "collapse-item" href = "<?= base_url('barangkeluar ') ?>">Barang Keluar</a>
-
-        <?php } ?>
+-->
       </div>
     </div>
   </li>
