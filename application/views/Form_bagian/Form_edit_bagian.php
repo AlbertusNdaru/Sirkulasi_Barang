611cc427d8e6a50d75ba7418f09c0b275ed8da49
Farishy25 <58514816+Farishy25@users.noreply.github.com>
2019-12-16 23:06:16 +0700

    Update Form_edit_bagian.php

--- application/views/Form_bagian/Form_edit_bagian.php
+++ application/views/Form_bagian/Form_edit_bagian.php
@@ -2,18 +2,18 @@
     <div class="card shadow">
         <div class="card-header border-0">
             <form name="fromaddbagian" action="<?= base_url('Controller_bagian/Controller_bagian/editbagian') ?>" method="post">
-                <h6 class="heading-small text-muted mb-4">Add Bagian</h6>
+                <h6 class="heading-small text-muted mb-4">Edit Bagian</h6>
                 <hr class="my-4" />
                 <div class="pl-lg-4">
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label class="form-control-label" for="input-address">Name Bagian</label>
-                                <input id="input-address" class="form-control form-control-alternative" name="name" placeholder="Name Bagian" value="<?=$editbagian->Name?>" type="text">
+                                <input id="input-address" class="form-control form-control-alternative" name="name" placeholder="Name Bagian" value="<?= $editbagian->Name ?>" type="text">
                             </div>
                         </div>
                     </div>
-                    <button type="submit" name="submitid" value="<?= $editbagian->id_bagian ?>" class="btn btn-primary">Add Data</button>
+                    <button type="submit" name="submitid" value="<?= $editbagian->id_bagian ?>" class="btn btn-primary">Update Data</button>
             </form>
         </div>
     </div>
